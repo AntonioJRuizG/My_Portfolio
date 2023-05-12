a111b963aef314833267abcdfb549e53d9474884
AntonioJesusRG <antoniogar129@gmail.com>
2023-05-12 12:05:33 +0200

    Add about section

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,9 +1,11 @@
+import About from '../components/about/about.js';
 import Gallery from '../components/gallery/gallery.jsx';
 import React from 'react';
 
 export default function App() {
   return (
     <>
+      <About></About>
       <Gallery></Gallery>
     </>
   );
---
+++ src/components/about/about.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section>
+      <article>
+        <h2>About</h2>
+        <p>
+          I am Antonio! I went from <span>painter</span> 🎨 to{' '}
+          <span>developer</span> 👩‍💻, deciding to change the canvas for the
+          screen. Although I miss the feel of a canvas, I'm moved to create
+          something new and exciting with every line of code I write.
+        </p>
+        <p>
+          I designed my first website when I was 16 years old. I didn't have
+          access to the necessary information to make the project go beyond a
+          static website that I could only see locally but that unsatisfied
+          curiosity has been the seed of my interest in knowing the complete
+          process of web design and what would end up moving me to study a full
+          stack bootcamp a few years later.
+        </p>
+        <p>
+          It has been a risky bet in personal, family and economic terms,
+          leaving my comfort zone in a drastic way but I couldn't feel more
+          satisfied with the results and knowing not only how to design a full
+          stack web, but also how to do it supported by testing and clean,
+          readable code.
+        </p>
+        <p>
+          From now on I know many <span>challenges</span> and
+          <span>great people</span> are waiting for me on the way.
+        </p>
+      </article>
+      <div>
+        <img src="../../../img/profile_picture.jpg" alt="Profile picture"></img>
+      </div>
+    </section>
+  );
+}
