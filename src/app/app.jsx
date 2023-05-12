1d397add19d1ac452c727f00ee3dc804f66abcab
AntonioJesusRG <antoniogar129@gmail.com>
2023-05-12 23:14:32 +0200

    Add home component

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,10 +1,12 @@
-import About from '../components/about/about.js';
+import Home from '../components/home/home.jsx';
+import About from '../components/about/about.jsx';
 import Gallery from '../components/gallery/gallery.jsx';
 import React from 'react';
 
 export default function App() {
   return (
     <>
+      <Home></Home>
       <About></About>
       <Gallery></Gallery>
     </>
---
+++ src/components/home/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './home.scss';
+
+export default function Home() {
+  return (
+    <section className="homeSection">
+      <h1> Hello, I'm Antonio</h1>
+      <p>Junior Frontend Developer</p>
+
+      <div>
+        <a>Download CV</a>
+        <a>GitHub</a>
+        <a>LinkedIn</a>
+      </div>
+    </section>
+  );
+}
