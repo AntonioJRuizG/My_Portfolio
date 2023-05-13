5969aced66eb5d4e13e5c18c9c0244354530306d
AntonioJesusRG <antoniogar129@gmail.com>
2023-05-13 13:48:08 +0200

    Style gallery and change home and about

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import Thumbnail from '../thumbnail/thumbnail.jsx';
 
+// @ts-ignore
+import styles from './gallery.module.scss';
+
 const projectList = [
   {
-    id: '1',
+    id: '001',
     name: 'Be Euphonium',
     imgName: 'be-euphonium',
-    description: 'Network',
-    link: 'https://beeuphonium.netlify.app/',
+    description:
+      'Music enthusiasts network to share a gallery of their favourite instruments, filter the gallery and registered users can add new ones, edit or remove them.',
+    stack:
+      'React | TypeScript | SASS | Redux | Node.js | Express | MongoDB | Jest | SonarQube',
+    linkWeb: 'https://beeuphonium.netlify.app/',
+    linkCode: 'https://github.com/AntonioJRuizG/Be-Euphonium_front',
   },
 ];
 
 export default function Gallery() {
   return (
-    <section>
-      <h1>Projects</h1>
-      <ul>
+    <section className={styles.gallerySection}>
+      <h2 className={styles.gallerySectionTitle}>Projects</h2>
+      <ul className={styles.galleryItems}>
         {projectList.map((item) => (
-          <li key={item.id} className="thumbnailContainer">
+          <li key={item.id} className={styles.thumbnailContainer}>
             <Thumbnail props={item}></Thumbnail>
           </li>
         ))}
