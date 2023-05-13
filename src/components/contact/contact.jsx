92314b99ac0a1fdb718dbdc053ebc20362a4f87c
AntonioJesusRG <antoniogar129@gmail.com>
2023-05-13 21:32:31 +0200

    Add contact component

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+// @ts-ignore
+import styles from './contact.module.scss';
+import MediaButton from '../button/button.jsx';
+import { LinksList } from '../home/home.jsx';
+
+export default function Contact() {
+  return (
+    <section className={styles.contactSection}>
+      <h2>Let's contact!</h2>
+
+      <article className={styles.article}>
+        <div>
+          <p>Contact form:</p>
+          <form
+            className={styles.contactForm}
+            name="contact"
+            // @ts-ignore
+            netlify
+          >
+            <input type="text" name="name" placeholder="Name"></input>
+
+            <textarea name="comment" placeholder="Comment" required></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className={styles.socialMedia}>
+          <p>Social media accounts:</p>
+          <ul>
+            {LinksList.map(
+              (item) =>
+                item.text !== 'Download CV' && (
+                  <li key={item.text}>
+                    <MediaButton props={item}></MediaButton>
+                  </li>
+                )
+            )}
+          </ul>
+        </div>
+      </article>
+    </section>
+  );
+}
