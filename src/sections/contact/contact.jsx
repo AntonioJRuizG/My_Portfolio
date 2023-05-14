a8b8b04d2670f039617c3f5c02b69fb205aeb860
AntonioJesusRG <antoniogar129@gmail.com>
2023-05-14 03:03:30 +0200

    Fix form config

--- src/sections/contact/contact.jsx
+++ src/sections/contact/contact.jsx
@@ -13,9 +13,14 @@ export default function Contact() {
       <article className={styles.article}>
         <div>
           <p>Contact form:</p>
-          <form className={styles.contactForm} name="contact" method="post">
-            <input type="hidden" name="contact" value="contact" />
-            <input type="text" name="name" placeholder="Name"></input>
+          <form
+            className={styles.contactForm}
+            name="contact"
+            action="/contact"
+            method="post"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="text" name="name" placeholder="Name" required></input>
             <textarea name="comment" placeholder="Comment" required></textarea>
             <button type="submit">Send</button>
           </form>
