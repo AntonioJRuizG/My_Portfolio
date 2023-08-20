7781cc738fccec08a3fea5ea611ffe99d5868e72
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-21 00:25:32 +0200

    Change src refs

--- src/components/imgsGrid/imgs-grid.jsx
+++ src/components/imgsGrid/imgs-grid.jsx
@@ -3,34 +3,34 @@ import React from 'react';
 import styles from './imgs-grid.module.scss';
 
 const techIcons = [
-  '../../../stack-icons/react.png',
-  '../../../stack-icons/angular.svg',
-  '../../../stack-icons/primeng-logo.png',
-  '../../../stack-icons/java.svg',
-  '../../../stack-icons/html.png',
-  '../../../stack-icons/css.png',
-  '../../../stack-icons/sass.png',
-  '../../../stack-icons/figma.png',
-  '../../../stack-icons/javascript.png',
-  '../../../stack-icons/typescript.png',
+  './stack-icons/react.png',
+  './stack-icons/angular.svg',
+  './stack-icons/primeng-logo.png',
+  './stack-icons/java.svg',
+  './stack-icons/html.png',
+  './stack-icons/css.png',
+  './stack-icons/sass.png',
+  './stack-icons/figma.png',
+  './stack-icons/javascript.png',
+  './stack-icons/typescript.png',
 
-  '../../../stack-icons/npm.svg',
-  '../../../stack-icons/git.png',
-  '../../../stack-icons/github.png',
-  '../../../stack-icons/mysql.svg',
-  '../../../stack-icons/nodejs.svg',
-  '../../../stack-icons/express.png',
-  '../../../stack-icons/mongodb.svg',
-  '../../../stack-icons/postman.png',
-  '../../../stack-icons/jwt.png',
-  '../../../stack-icons/netlify.png',
-  '../../../stack-icons/firebase.svg',
-  '../../../stack-icons/redux.png',
-  '../../../stack-icons/jest.png',
-  '../../../stack-icons/react-testing.png',
-  '../../../stack-icons/visual.png',
-  '../../../stack-icons/IntelliJ_IDEA.svg',
-  '../../../stack-icons/eclipse.png',
+  './stack-icons/npm.svg',
+  './stack-icons/git.png',
+  './stack-icons/github.png',
+  './stack-icons/mysql.svg',
+  './stack-icons/nodejs.svg',
+  './stack-icons/express.png',
+  './stack-icons/mongodb.svg',
+  './stack-icons/postman.png',
+  './stack-icons/jwt.png',
+  './stack-icons/netlify.png',
+  './stack-icons/firebase.svg',
+  './stack-icons/redux.png',
+  './stack-icons/jest.png',
+  './stack-icons/react-testing.png',
+  './stack-icons/visual.png',
+  './stack-icons/IntelliJ_IDEA.svg',
+  './stack-icons/eclipse.png',
 ];
 
 export default function ImageGrid() {
@@ -41,8 +41,8 @@ export default function ImageGrid() {
           key={index}
           src={icon}
           alt={`Stack icon ${index}`}
-          width={30}
-          height={30}
+          width={45}
+          height={45}
         />
       ))}
     </div>
