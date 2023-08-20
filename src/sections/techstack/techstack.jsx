baadbbb290bdbd1df260aabfb80706aad88d1bfb
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-20 22:56:46 +0200

    Add tech stack icons grid

--- src/sections/techstack/techstack.jsx
+++ src/sections/techstack/techstack.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 // @ts-ignore
 import styles from './techstack.module.scss';
+import ImageGrid from '../../components/imgsGrid/imgs-grid';
 
 export default function TechStack() {
   return (
     <section id="techstack" className={styles.techStackSection}>
       <h2>Tech Stack</h2>
+      <ImageGrid></ImageGrid>
     </section>
   );
 }
